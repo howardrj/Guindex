870804d18c2ae6dc2f0e30acd100cdbb7009a0f8
howardrj <howardrj@tcd.ie>
2018-02-16 09:22:54 +0000

    Implemented DRF throttling.

--- GuindexProject/GuindexProject/settings.py
+++ GuindexProject/GuindexProject/settings.py
@@ -263,10 +263,19 @@ LOGGING = {
     }
 }
 
+# REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '100/day'
+    }
 }
 
 # Telegram API
