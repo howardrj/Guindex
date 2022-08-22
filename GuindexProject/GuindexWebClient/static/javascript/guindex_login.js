9064f625d97a2ac46e502843b212ff373f3501b6
Robert Howard <howardrj@tcd.ie>
2022-08-22 22:18:50 +0000

    Issue 33 - Firebase logout now working

--- GuindexProject/GuindexWebClient/static/javascript/guindex_login.js
+++ GuindexProject/GuindexWebClient/static/javascript/guindex_login.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
 
 function guindex_login_init_firebase ()
 {
@@ -118,6 +117,45 @@ class GuindexUserStateLoggedIntoGuindex extends GuindexUserState
 
     init ()
     {
+        // Show pending contributions tab 
+        if (this.user.is_staff_member)
+            document.getElementById('pending_contributions_li').style.display = 'list-item';
+
+        var page_contents = document.getElementsByClassName('page_content');
+
+        for (var i = 0; i < page_contents.length; i++)
+        {
+            page_contents[i].dispatchEvent(new Event('on_login'));
+        }
+
+        // Set login status link to display username (i.e. top right corner of GUI)
+        var login_link = document.getElementById('login_link');
+        login_link.innerHTML = this.user.username;
+
+        // Toggle log ini/out display messages
+        document.getElementById('logged_out_modal_header').style.display = 'none';
+        document.getElementById('logged_in_modal_header').style.display = 'block';
+
+        document.getElementById('logged_out_modal_body').style.display = 'none';
+        document.getElementById('logged_in_modal_body').style.display = 'block';
+        document.getElementById('logged_in_modal_body_username').innerHTML = this.user.username;
+
+        document.getElementById('logged_out_modal_footer').style.display = 'none';
+        document.getElementById('logged_in_modal_footer').style.display = 'block';
+    
+        // Add logout button event listener
+        $(document).on('click', '#logout_button', function () {
+
+            toggleLoader(this);
+
+            firebase.auth().signOut().then(function() {
+                // Sign-out successful.
+                location.reload();
+            }).catch(function(error) {
+                // An error happened
+               toggleLoader(this);
+           });    
+        });
     }
 
     deinit ()
@@ -148,6 +186,7 @@ class GuindexUser
         this.user_id = 0;
         this.url_base = location.protocol + '//' + location.hostname + ':' + location.port;
         this.api_base = this.url_base + '/api/';
+        this.login_method = 'google';
     }
 
     async request_access_token ()
@@ -168,10 +207,11 @@ class GuindexUser
         let data = await response.json();
 
         this.guindex_access_token = data['results'][0]['key'];
-        this.is_staff_member = (data['results'][0]['isStaff'] == 'True')
-        this.user_id = data['results'][0]['user']
+        this.is_staff_member = (data['results'][0]['isStaff'] == 'True');
+        this.user_id = data['results'][0]['user'];
+        this.username = data['results'][0]['username'];
 
-        this._change_state(GuindexUserStates.LOGGED_INTO_GUINDEX)
+        this._change_state(GuindexUserStates.LOGGED_INTO_GUINDEX);
     }
 
     check_firebase_auth_status ()
@@ -193,6 +233,9 @@ class GuindexUser
                 break;
             case GuindexUserStates.AUTHENTICATED_BY_PROVIDER:
                 new_state = new GuindexUserStateAuthenticatedByProvider(this);
+                break;
+            case GuindexUserStates.LOGGED_INTO_GUINDEX:
+                new_state = new GuindexUserStateLoggedIntoGuindex(this);
                 break;
             default:
                 throw new Error("Cannot transition to state with ID %d", state_id);
